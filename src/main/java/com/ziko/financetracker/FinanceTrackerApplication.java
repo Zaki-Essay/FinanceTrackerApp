febfc10a22a9e3468bceea162d15c2966496c971
Zaki-Essay <zakaressaiydy@gmail.com>
2024-08-26 20:05:55 +0100

    first commit

---
+++ src/main/java/com/ziko/financetracker/FinanceTrackerApplication.java
@@ -0,0 +1,14 @@
+package com.ziko.financetracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinanceTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinanceTrackerApplication.class, args);
+		System.out.println("the project is working");
+	}
+
+}
